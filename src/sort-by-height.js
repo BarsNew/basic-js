3c446b9c4eecec425c5d12ab778ab015ad3e1919
BarsNew <Bars11021988@mail.ru>
2023-04-17 23:26:40 +0300

    feat: tasks solution 21 (Mon, 17 April 2023,23:26:40)

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight( /*arr */) {
-  throw new NotImplementedError('Not implemented');
+function sortByHeight( arr ) {
+  //throw new NotImplementedError('Not implemented');
 
+  let repeatArr = [];
+  let checkFalse = {};
+  let check = {};
+
+  let nums = arr.filter(function(item, index) {
+      if (arr.indexOf(item) === arr.lastIndexOf(item) && arr.indexOf(item) === index) {
+          check[item] = true;
+          return true;
+      }
+      else {
+          check[item] = false;
+          checkFalse[item] = false;
+          repeatArr.push(item);
+          return false;
+      }
+  }).sort(function(a, b) {
+      return a - b;
+  });
+
+
+  if (!Object.values(check)[0]) {
+      let newArr = [];
+      for (let item of repeatArr) {
+          newArr.push(+item)
+      }
+      for (let item of nums) {
+          newArr.push(+item)
+      }
+      
+      return newArr;
+  }
+
+  arr.forEach((item, index) => {
+      if (check[item]) {
+          let numb = nums.shift();
+          arr[index] = +numb;
+      }
+  });
+
+  return arr;
 }
 
 module.exports = {
